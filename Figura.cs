ba097c6f8ef29fc3fc96ec4da0f1bde32d158195
maranhasc <cmaranhas@gmail.com>
2023-10-07 13:22:28 +0200

    prueba2

---
+++ Figura.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Dynamic;
+
+namespace p2_2_Maranhas_Carmen;
+
+internal abstract class Figura
+{
+  protected Color _color;
+
+  public abstract double GetArea();
+
+}
