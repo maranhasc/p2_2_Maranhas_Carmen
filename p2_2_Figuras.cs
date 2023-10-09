f512c783cd5835b61906c1ed6f1b6c3775ebdd5d
maranhasc <cmaranhas@gmail.com>
2023-10-09 08:52:20 +0200

    ultimo

--- p2_2_Figuras.cs
+++ p2_2_Figuras.cs
@@ -14,20 +14,26 @@ public class P2_2_Figuras
     double suma = 0;
 
     //llamadas a las clases
-    List<Figura> figuras = new List<Figura>();
+    List<Figura> lista_figuras = new List<Figura>();
     Rectangulo r1 = new Rectangulo(4,5);
-    figuras.Add(r1);
+    lista_figuras.Add(r1);
     Circulo c1 = new Circulo(4);
-    figuras.Add(c1);
+    lista_figuras.Add(c1);
     Triangulo t1 = new Triangulo(4);
-    figuras.Add(t1);
+    lista_figuras.Add(t1);
     t1 = new Triangulo(5);
-    figuras.Add(t1);
+    lista_figuras.Add(t1);
+    Triangulo t2 = new Triangulo(5);
+    lista_figuras.Add(t2);
+    Rectangulo r2 = new Rectangulo(5,6);
+    lista_figuras.Add(r2);
+    Circulo c2 = new Circulo(5);
+    lista_figuras.Add(c2);
     
 
 
 
-    foreach (Figura figura in figuras){
+    foreach (Figura figura in lista_figuras){
     Console.WriteLine(figura.ToString());
     Console.WriteLine($"El area es: {figura.GetArea()}");
     Console.WriteLine();
